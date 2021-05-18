d9ee5cb44e7ea249d7e883342897d3700a32e7ff
Victor-Chris <victorchris51@gmail.com>
2021-05-18 16:39:42 +0300

    Moved Data Access Object to root folder and created the restaurants controller APi

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,7 +1,9 @@
 import express from "express";
+import RestaurantController from "./restaurants.controller.js";
 
 const router = express.Router();
 
-router.route("/").get((req, res) => res.send("Server Test"));
+//router.route("/").get((req, res) => res.send("Server Test"));
+router.route("/").get(RestaurantController.apiGetRestaurants);
 
 export default router;
