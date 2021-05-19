ac38a645f9dc252414398b8e7c270cdba9fb67c5
Victor-Chris <victorchris51@gmail.com>
2021-05-19 13:44:02 +0300

    Added Reviews Data Access Object and Controller

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,9 +1,16 @@
 import express from "express";
+import ReviewsController from "./reviews.controller.js";
 import RestaurantController from "./restaurants.controller.js";
 
 const router = express.Router();
 
 //router.route("/").get((req, res) => res.send("Server Test"));
 router.route("/").get(RestaurantController.apiGetRestaurants);
+
+router
+  .route("/review")
+  .post(ReviewsController.apiPostReview)
+  .put(ReviewsController.apiUpdateReview)
+  .delete(ReviewsController.apiDeleteReview);
 
 export default router;
